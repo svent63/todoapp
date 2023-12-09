0d9ce934412658f0d8461c7c305d4c9a7d2354e1
sventer <vent0212@protonmail.com>
2023-12-09 08:34:19 +0200

    display todo items in list read from db

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,58 +1,34 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
+import { useIndexedDB } from 'react-indexed-db-hook';
+
+type TodoItemRecord = {
+    id: number;
+    complete: boolean;
+    task: string;
+};
 
 const TodoList = () => {
+    const [todoItems, setTodoItems] = useState<TodoItemRecord[]>([]);
+    const { getAll } = useIndexedDB('todo');
+
+    useEffect(() => {
+        getAll().then((todoItemFromDb) => {
+            setTodoItems(todoItemFromDb);
+        });
+    }, []);
+
+    const todoItemList = todoItems.map((item, i) => {
+        return (
+            <li key={i}>
+                <input type='checkbox' name='' id='' checked={item.complete} />
+                {item.task}
+            </li>
+        );
+    });
+
     return (
         <Fragment>
-            <ul>
-                <li>
-                    <input type='checkbox' name='' id='' className='checkbox' />
-                    List Item
-                </li>
-                <li>
-                    <input type='checkbox' name='' id='' className='checkbox' />
-                    List Item
-                </li>
-                <li>
-                    <input type='checkbox' name='' id='' className='checkbox' />
-                    List Item
-                </li>
-                <li>
-                    <input type='checkbox' name='' id='' className='checkbox' />
-                    List Item
-                </li>
-                <li>
-                    <input type='checkbox' name='' id='' className='checkbox' />
-                    List Item
-                </li>
-                <li>
-                    <input type='checkbox' name='' id='' className='checkbox' />
-                    List Item
-                </li>
-                <li>
-                    <input type='checkbox' name='' id='' className='checkbox' />
-                    List Item
-                </li>
-                <li>
-                    <input type='checkbox' name='' id='' className='checkbox' />
-                    List Item
-                </li>
-                <li>
-                    <input type='checkbox' name='' id='' className='checkbox' />
-                    List Item
-                </li>
-                <li>
-                    <input type='checkbox' name='' id='' className='checkbox' />
-                    List Item
-                </li>
-                <li>
-                    <input type='checkbox' name='' id='' className='checkbox' />
-                    List Item
-                </li>
-                <li>
-                    <input type='checkbox' name='' id='' className='checkbox' />
-                    List Item
-                </li>
-            </ul>
+            <ul>{todoItemList}</ul>
         </Fragment>
     );
 };
