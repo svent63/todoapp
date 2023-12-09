ce6553673b11990f22a7d661f0ebb3decae7dac8
svent63 <80626690+svent63@users.noreply.github.com>
2023-12-09 06:32:45 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# parcel-react-template
+
+Template for building React apps with the Parcel Bundler.
+
+Clone the repo and run `npm install`
---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+import App from './App';
+
+const app = document.getElementById('app');
+if (app) {
+    const root = createRoot(app);
+    root && root.render(<App />);
+}
