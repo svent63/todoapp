b808b0dce55d39f5d19ca1ed2faf73b385cacf8f
sventer <vent0212@protonmail.com>
2023-12-09 09:10:48 +0200

    update database on checkbox change

--- src/components/checkbox/Checkbox.tsx
+++ src/components/checkbox/Checkbox.tsx
@@ -1,17 +1,22 @@
 import React, { useState } from 'react';
+import { useIndexedDB } from 'react-indexed-db-hook';
 
 type CheckboxProps = {
+    id: number;
     isChecked: boolean;
+    task: string;
 };
 
-const Checkbox = ({ isChecked }: CheckboxProps) => {
+const Checkbox = ({ id, isChecked, task }: CheckboxProps) => {
     const [checked, setChecked] = useState(isChecked);
+    const { update } = useIndexedDB('todo');
 
-    const handleChange = () => {
+    const handleChange = (id: number) => {
         setChecked(!checked);
+        update({ id: id, complete: !checked, task: task });
     };
 
-    return <input type='checkbox' name='' id='' checked={checked} onChange={handleChange} />;
+    return <input type='checkbox' name='' id='' checked={checked} onChange={() => handleChange(id)} />;
 };
 
 export { Checkbox };
